6cf34fd84b0910906d7c01fca2f5f744476adede
unknown <kevin133557@gmail.com>
2024-09-17 12:02:56 -0400

    Mensaje de confirmación

--- dist/index.js
+++ dist/index.js
@@ -13,7 +13,7 @@ server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
 //configuracion de cors
 server.app.use((0, cors_1.default)({
-    origin: "https://cliente-socket1-s45y.vercel.app/",
+    origin: "https://cliente-socket1-s45y.vercel.app",
     credentials: true,
 }));
 server.app.use("/", router_1.router);
