74347e70880c0be3b898dc3ff29c65244f15b890
unknown <kevin133557@gmail.com>
2024-09-17 16:18:28 -0400

    Tu mensaje de confirmación

--- classes/server.ts
+++ classes/server.ts
@@ -19,7 +19,7 @@ export default class Server {
     // this.io = new SocketOIServer(this.httpServer);
     this.io = new SocketOIServer(this.httpServer, {
       cors: {
-        origin: "https://cliente-socket1-s45y.vercel.app",
+        origin: "https://cliente-final-indol.vercel.app",
         methods: ["GET", "POST"],
       },
     });
